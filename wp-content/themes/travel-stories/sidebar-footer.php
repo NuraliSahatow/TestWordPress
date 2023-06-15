d1a80b563cc2fe6c6660578958c38807512699b0
Nurali Sahatow <61908851+NuraliSahatow@users.noreply.github.com>
2023-06-15 18:06:53 +0300

    add via files

---
+++ wp-content/themes/travel-stories/sidebar-footer.php
@@ -0,0 +1,31 @@
+<?php /**
+ * The footer sidebar containing the main widget area
+ *
+ * Displays on widgets.
+ *
+ * If no active widgets are in this footer sidebar, hide it completely.
+ *
+ * @subpackage Travel Stories
+ * @since      Travel Stories 1.0
+ */
+if ( is_active_sidebar( 'footer_sidebar' ) ) {
+	dynamic_sidebar( 'footer_sidebar' );
+} else { /*If sidebar no active display next widget */
+	$args     = array(
+		'before_widget' => '<div class="footer_widget %s">',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h3 class="footer_title">',
+		'after_title'   => '</h3>',
+	);
+	$instance = array();
+	the_widget( 'WP_Widget_Archives', $instance, $args );
+	the_widget( 'WP_Widget_Categories', $instance, $args );
+	the_widget( 'WP_Widget_Recent_Posts', $instance, $args );
+	the_widget( 'WP_Widget_Recent_Comments', $instance, $args );
+	
+} ?>
+<form id="searchform" method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+	<input id="travel-stories-search-box" class="textbox" type="text" name="s" placeholder="<?php esc_attr_e( 'Enter search keyword', 'travel-stories' ); ?>" value="<?php echo get_search_query(); ?>">
+</form><!-- .searchform -->
+
+
